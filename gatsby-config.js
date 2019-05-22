d3f28222a4188677e5ff67d86fafba05d59f36e6
Derrick <dbozich1@gmail.com>
2019-05-21 23:47:02 -0400

    init

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,5 +5,15 @@
  */
 
 module.exports = {
-  /* Your site config here */
+  siteMetadata: {
+    title: `McKinstry Law Firm`,
+  },
+  plugins: [
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
+  ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,22 @@
 import React from "react"
+import { graphql } from "gatsby"
+import Layout from "../components/layout"
 
-export default () => <div>Under Construction...</div>
+export default ({ data }) => (
+  <Layout>
+    <h1>{data.site.siteMetadata.title}</h1>
+    <p>
+      Site under construction...
+    </p>
+  </Layout>
+)
+
+export const query = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+  `
