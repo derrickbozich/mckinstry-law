89620c43cd78a644ac221c95a6f370b98d8c5c27
Derrick <dbozich1@gmail.com>
2021-01-20 11:11:19 -0700

    added GA

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,7 +31,18 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
-
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "UA-187524931-1", // Google Analytics / GA
+          "G-M039N3ZXKW",
+          // "AW-CONVERSION_ID", // Google Ads / Adwords / AW
+          // "DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
+        ],
+      },
+    },
 
     {
       resolve: `gatsby-plugin-sharp`,
